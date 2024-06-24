c2938168755c89e4da794b2bf9c91fb432e03a77
fxdickys1602 <95075990+fxdickys1602@users.noreply.github.com>
2024-06-24 17:40:49 +0700

    update 24/06/2024

---
+++ src/scripts/home.js
@@ -0,0 +1,11 @@
+import data from '../public/data/DATA.json';
+
+const home = () => {
+    let myArr = data;
+    const listElement = dataListContainerElement.querySelector('note-list');
+    
+
+    console.log(myArr);
+}
+
+export default home;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,10 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import home from './home.js'
 
-console.log('Hello Coders! :)');
+// console.log('Hello Coders! :)');
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    home();
+})
